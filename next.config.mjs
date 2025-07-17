7a1ce18809a85ef74afcd2b5f27da762c6d6bd48
VSaini11 <116022904+VSaini11@users.noreply.github.com>
2025-07-17 15:42:34 +0530

    just commit

--- next.config.mjs
+++ next.config.mjs
@@ -8,9 +8,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: process.env.NODE_ENV === 'production'
-                    ? 'https://safe-drop-server.up.railway.app/api/:path*'
-                    : 'http://localhost:3001/api/:path*'
+                destination: '/api/:path*'
             }
         ]
     }
